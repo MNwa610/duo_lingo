6f38d87f76c46b200fd4f69f45556984d3216ee4
Михаил Цепляев <mnwa@Noutbuk-Mihail-4.local>
2025-11-25 17:40:34 +0300

    fix

--- src/App.js
+++ src/App.js
@@ -1,16 +1,12 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import WordTrainer from './components/WordTrainer';
+import './App.css';
 
 function App() {
   return (
-    <Router basename="/your-repo-name">
-      <div className="App">
-        <Routes>
-          <Route path="/" element={<WordTrainer />} />
-        </Routes>
-      </div>
-    </Router>
+    <div className="App">
+      <WordTrainer />
+    </div>
   );
 }
 
